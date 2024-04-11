b3ef24cb12d8e8c6f382b4457d51c51331a504a8
regina-cht <regina.ch13@gmail.com>
2024-04-10 18:00:10 -0600

    Optimized images and added animations to enhance text presentation.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.style.width = mobileMenu.style.width === '100%' ? '0' : '100%';
+}
+
+function cerrarNav() {
+  document.getElementById('mobile-menu').style.width = '0';
+}
